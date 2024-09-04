8d167839ca3c59c6160335432d09b91574940742
liuwen <liuwen@nx.lvtu.in>
2024-09-04 11:00:51 +0800

    refactor: update Axum server setup and imports

--- vibe-server/src/main.rs
+++ vibe-server/src/main.rs
@@ -1,22 +1,22 @@
 use axum::{
     routing::get,
     Router,
+    Server,
 };
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    // build our application with a route
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }));
-
-    // run our app with hyper
+    // run our app with hyper, listening on all interfaces
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
     
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }

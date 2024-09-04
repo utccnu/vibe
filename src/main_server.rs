5dcdabb97b3d7f853513fbae0b46d52f217ce2b7
liuwen <liuwen@nx.lvtu.in>
2024-09-04 10:42:47 +0800

    feat: set up basic Axum server with Hello World route

--- src/main_server.rs
+++ src/main_server.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, World!");
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }

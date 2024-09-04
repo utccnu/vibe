884dcc0174f6d83fdd69e076582b0404c2a97ee2
liuwen <liuwen@nx.lvtu.in>
2024-09-04 10:45:29 +0800

    refactor: remove vibe_core dependency and update ModelContext

--- vibe-server/src/setup.rs
+++ vibe-server/src/setup.rs
@@ -1,15 +1,14 @@
 use std::sync::Arc;
 use tokio::sync::Mutex;
-use vibe_core::whisper::WhisperContext;
 
 pub struct ModelContext {
-    pub whisper: Arc<Mutex<Option<WhisperContext>>>,
+    pub whisper: Arc<Mutex<Option<String>>>,
 }
 
 impl ModelContext {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
         Ok(Self {
-            whisper: Arc::new(Mutex::new(None)),
+            whisper: Arc::new(Mutex::new(Some("Placeholder for WhisperContext".to_string()))),
         })
     }
 }
